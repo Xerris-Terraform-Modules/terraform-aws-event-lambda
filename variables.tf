2d9b2b4c1d797bb0a7eb8cd2e8190161ff478d59
Jason Wiker <jason.wiker@gmail.com>
2021-03-15 17:25:49 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,71 +1,122 @@
-////////Base Lambda Variables
-variable "entrypoint" {}
-variable "command" {
-    default = []
+////////Base Lambda Variables//////////////////////////////////////////////////
+variable "entrypoint" {
+    type        = string
+    description = "The entrypoint of the lambda."
 }
-variable "env"{}
-variable "image" {}
-variable "function_name" {}
-variable "description" {}
+variable "command" {
+    default     = []
+    type        = list(string)
+    description = "The container commands for the lambda. Probably will depricate so don't use this"
+}
+variable "env" {
+    type        = string
+    description = "The environment of the lambda. (dev, stage, prod)"
+}
+variable "image" {
+    type        = string
+    description = "The id of the image for the lambda."
+}
+variable "function_name" {
+    type        = string
+    description = "The name for this lambda."
+}
+variable "description" {
+    type        = string
+    description = "The description for this lambda."
+}
 
-////////S3 Event Variables
+////////S3 Event Variables//////////////////////////////////////////////////
 variable "enable_s3_event" {
-    default = false
+    default     = false
+    type        = bool
+    description = "If you want this lambda to be triggered by an S3 upload event."
 }
 variable "timeout" {
-    default = 30
+    default     = 30
+    type        = number
+    description = "Timeout of the lambda."
 }
 variable "memory_size" {
-    default = 128
+    default     = 128
+    type        = number
+    description = "Memory size of the lambda."
 }
 variable "bucket_event_arn" {
-     default = null
+    default     = null
+    type        = string
+    description = "Arn of the bucket you want to trigger on."
 }
 variable "bucket_event_id" {
-     default = null
+    default    = null
+    type        = string
+    description = "Id of the bucket you want to trigger on."
 }
 ////////SQS Event Variables
 variable "enable_sqs_event" {
-    default = false
+    default     = false
+    type        = bool
+    description = "If you want to trigger the lambda on an item in an SQS Queue event."
 }
 variable "sqs_event_arn" {
-     default = null
-}
-
-////////Private Networking Variables
-variable "subnet_ids" {
-        default = null
-}
-variable "vpc_security_group_ids" {
-        default = null
-}
-///////IAM Variables
-variable "s3_readwrite_arn_iam_list" {
-    default = []
-}
-variable "s3_read_arn_iam_list" {
-    default = []
-}
-variable "sqs_arn_iam_list" {
-    default = []
-}
-variable "dynamodb_readwrite_arn_iam_list" {
-    default = []
-}
-variable "dynamodb_read_arn_iam_list" {
-    default = []
-}
-variable "secretsmanager_arn_iam_list" {
-    default = []
-}
-variable "lambda_arn_iam_list" {
-    default = []
+    default     = null
+    type        = string
+    description = "Arn of the SQS Queue you want to trigger on."
 }
 ///////Chron Variables
-
 variable "enable_chron" {
-    default = false
+    default     = false
+    type        = bool
+    description = "If you want to enable the lambda to be triggered on a regular(chron) scheduale"
 }
 variable "cron_expression" {
-    default = null
+    default     = null
+    type        = string
+    description = "Chron expression you want the lambda to be triggered on"
+}
+///////IAM Variables//////////////////////////////////////////////////
+variable "s3_readwrite_arn_iam_list" {
+    default     = []
+    type        = list(string)
+    description = "List of the s3 arns you want the lambda to have read/write iam access to."
+}
+variable "s3_read_arn_iam_list" {
+    default     = []
+    type        = list(string)
+    description = "List of the s3 arns you want the lambda to have read iam access to."
+}
+variable "sqs_arn_iam_list" {
+    default     = []
+    type        = list(string)
+    description = "List of the sqs arns you want the lambda to have iam access to."
+}
+variable "dynamodb_readwrite_arn_iam_list" {
+    default     = []
+    type        = list(string)
+    description = "List of the dynamodb arns you want the lambda to have read/write iam access to."
+}
+variable "dynamodb_read_arn_iam_list" {
+    default     = []
+    type        = list(string)
+    description = "List of the dynamodb arns you want the lambda to have read iam access to."
+}
+variable "secretsmanager_arn_iam_list" {
+    default     = []
+    type        = list(string)
+    description = "List of the secretsmanager arns you want the lambda to have iam access to."
+}
+variable "lambda_arn_iam_list" {
+    default     = []
+    type        = list(string)
+    description = "List of the lambda arns you want the lambda to have iam access to. Probably will depricate, use with caution"
+}
+////////Private Networking Variables//////////////////////////////////////////////////
+variable "subnet_ids" {
+    default     = null
+    type        = list(string)
+    description = "List of the subnet ids you want the lambda to reside in."
+}
+variable "vpc_security_group_ids" {
+    default     = null
+    type        = list(string)
+    description = "List of the security group ids you want the lambda to reside in."
 }
